cd15d73bdb29121283a624671f47f691727117d7
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-03 15:34:53 -0700

    Update HomePage.js

--- src/pages/Public/HomePage.js
+++ src/pages/Public/HomePage.js
@@ -1,4 +1,4 @@
-export default function HomePage() {
+export default function HomePage({ setPage }) {
   return (
     <div>
 
@@ -11,12 +11,20 @@ export default function HomePage() {
         <div className="title">Next Game</div>
         <div className="sub">Steelers vs Raiders</div>
         <div className="sub">April 12 • 11:30 AM</div>
-        <button className="button">View Schedule</button>
+
+        <button
+          className="button"
+          onClick={() => setPage("schedule")}
+        >
+          View Schedule
+        </button>
       </div>
 
       <div className="card">
         <div className="title">Announcements</div>
-        <div className="sub">Season starts this weekend!</div>
+        <div className="sub">
+          Season starts April 11th!
+        </div>
       </div>
 
     </div>
