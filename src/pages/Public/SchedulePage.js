896229c7423118c3d02700872c39c5281f386a01
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-03 14:37:51 -0700

    Update SchedulePage.js

--- src/pages/Public/SchedulePage.js
+++ src/pages/Public/SchedulePage.js
@@ -1,8 +1,21 @@
 export default function SchedulePage() {
   return (
     <div>
+
       <h2>Game Schedule</h2>
-      <p>(Schedule will load here)</p>
+
+      <div className="card">
+        <strong>Steelers vs Raiders</strong>
+        <p>April 12 • 11:30 AM</p>
+        <p>Field 4</p>
+      </div>
+
+      <div className="card">
+        <strong>Cowboys vs Eagles</strong>
+        <p>April 12 • 12:30 PM</p>
+        <p>Field 2</p>
+      </div>
+
     </div>
   );
 }
