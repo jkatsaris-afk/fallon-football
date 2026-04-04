d3c76af5b921f9a43443d1e52187cafbada2add5
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-03 17:21:20 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://qfgxbzqhwpscjpflxqfs.supabase.co";
-const supabaseKey = "YOUR_PUBLIC_KEY";
+const supabaseUrl = 'https://qfgxbzqhwpscjpflxqfs.supabase.co'
+const supabaseAnonKey = 'sb_publishable_lfyWSkFOrrNDEqwM6OuVJw_2Yr4sYZK'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
