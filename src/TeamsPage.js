0b05cafa1fd9b75010a417600afd3b97fa7474a3
jkatsaris-afk <jkatsaris@pcsdnv.com>
2026-04-13 12:01:22 -0700

    Create TeamsPage.js

---
+++ src/TeamsPage.js
@@ -0,0 +1,25 @@
+export default function TeamsPage() {
+  return (
+    <div>
+
+      <div className="card">
+        <div className="title">Steelers</div>
+        <div className="sub">Coach: John Smith</div>
+        <button className="button">View Team</button>
+      </div>
+
+      <div className="card">
+        <div className="title">Raiders</div>
+        <div className="sub">Coach: Mike Jones</div>
+        <button className="button">View Team</button>
+      </div>
+
+      <div className="card">
+        <div className="title">Cowboys</div>
+        <div className="sub">Coach: Steve Brown</div>
+        <button className="button">View Team</button>
+      </div>
+
+    </div>
+  );
+}
